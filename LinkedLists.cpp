d5024c01e4350f2416769e7fcb7b51df8e98b626
David-Szczepanik <Davidszczepanik@seznam.cz>
2024-05-24 15:02:48 +0200

    insertAfter

--- LinkedLists.cpp
+++ LinkedLists.cpp
@@ -35,12 +35,26 @@ void insertAtTheEnd(Node** head, int newValue) {
     return;
   }
   //3. Find the last node
-  Node*last = *head;
-  while(last->Next!=nullptr) {
+  Node* last = *head;
+  while (last->Next != nullptr) {
     last = last->Next;
   }
   //4. Insert newNode after last node (at the end)
-  last->Next = newNode;
+  last->Next = newNode; // last node will point to the newNode
+}
+
+void insertAfter(Node* previous, int newValue) {
+  //1. Check if previous node is NULL
+  if (previous == nullptr) {
+    cout << "Previous can not be NULL";
+    return;
+  }
+  //2. Prepare a newNode
+  Node* newNode = new Node();
+  newNode->Value = newValue;
+  //3. Insert newNode after previous
+  newNode->Next = previous->Next;
+  previous->Next = newNode;
 }
 
 void LinkedLists() {
@@ -55,11 +69,15 @@ void LinkedLists() {
   third->Value = 3;
   third->Next = nullptr;
 
+
   insertAtTheFront(&head, -0);
   insertAtTheFront(&head, -1);
 
   insertAtTheEnd(&head, 4);
   insertAtTheEnd(&head, 5);
+
+  insertAfter(head, -10);
+  insertAfter(second, -5);
 
   printList(head);
 }
